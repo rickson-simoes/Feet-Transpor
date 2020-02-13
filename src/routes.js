1a23c867459f092052487982f7ac0efff79a716d
Rickson <rickson.simoes@hotmail.com>
2020-02-13 16:20:12 -0300

    :hammer: Loader de Model - Finalizado

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ dude: 'theeee fuck' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Rickson',
+    email: 'rick@teste.com',
+    password_hash: '12345678',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
